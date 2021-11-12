68bfada0bb40b52b2db8eb01bb4e9908d2dc6241
leeZn30 <55116981+leeZn30@users.noreply.github.com>
2021-11-12 14:19:31 +0900

    CatnipFarm 구현중

--- Assets/Scripts/Ayeon/Feed.cs
+++ Assets/Scripts/Ayeon/Feed.cs
@@ -6,6 +6,7 @@ public class Feed : MonoBehaviour
 {
 
     public Vector3 targetPosition = Vector3.zero;
+    public float attackDmg;
 
     // Start is called before the first frame update
     void Start()
@@ -16,7 +17,7 @@ public class Feed : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.Translate(targetPosition * Time.deltaTime * 5.0f);
+        transform.Translate(targetPosition * Time.deltaTime * 10.0f);
 
         // 나와 부모의 사이가 일정거리 도달하면 삭제
         float distance = Vector3.Distance(transform.position, transform.parent.position);
