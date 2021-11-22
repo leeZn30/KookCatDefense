5992af9373acfb52d222012c2e5a8a16f6aa942d
Kim Seogyoung <55120754+Kimseogyoung@users.noreply.github.com>
2021-11-22 21:47:10 +0900

    게임씬 병합, tmpViewer GameManager코드 수정

--- Assets/Scripts/02_Game/TextTMPViewer.cs
+++ Assets/Scripts/02_Game/TextTMPViewer.cs
@@ -13,8 +13,6 @@ public class TextTMPViewer : MonoBehaviour
     private TextMeshProUGUI textWaveCount;
     [SerializeField]
     private TextMeshProUGUI textStage;
-    [SerializeField]
-    private GameManager gameManager;
     // Start is called before the first frame update
     void Start()
     {
@@ -24,9 +22,9 @@ public class TextTMPViewer : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        textPlayerGold.text = gameManager.coin.ToString();
-        textEnemyCount.text = gameManager.EnemyCnt.ToString();
-        textWaveCount.text = (gameManager.WaveNum+1).ToString();
-        textStage.text = "Stage " + (gameManager.mapIdx+1).ToString();
+        textPlayerGold.text = GameManager.Instance.coin.ToString();
+        textEnemyCount.text = GameManager.Instance.EnemyCnt.ToString();
+        textWaveCount.text = (GameManager.Instance.WaveNum + 1).ToString();
+        textStage.text = "Stage " + (GameManager.Instance.mapIdx+1).ToString();
     }
 }
