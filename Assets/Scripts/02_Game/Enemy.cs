3d02851d8fedbb44cadedf148a2134b47696f8d4
Kim Seogyoung <55120754+Kimseogyoung@users.noreply.github.com>
2021-11-27 16:17:09 +0900

    enemy 벽 공격 방식 변경, hitRange로 거리조절

--- Assets/Scripts/02_Game/Enemy.cs
+++ Assets/Scripts/02_Game/Enemy.cs
@@ -15,6 +15,8 @@ public class Enemy : MonoBehaviour
     public float attackSpeed;
 
     public float speed;
+    public float hitRange;
+    public LayerMask layerMask;
 
     [SerializeField]
     private float currentSpeed;
@@ -24,7 +26,6 @@ public class Enemy : MonoBehaviour
     private float curAffection = 0;
     public Transform affection_bar;
     public GameObject objAffection_bar;
-    public GameObject enemyAttackRangeObj;
 
     public bool isDead=false;
     
@@ -33,13 +34,14 @@ public class Enemy : MonoBehaviour
     private List<Transform> wayPoints;
     private int currentWayPointIdx=0;
     
-    private Transform transformAttackRange;
-    private EnemyAttackRange enemyAttackRange;
     private Rigidbody2D rigidbody2D;
     protected Animator animator;
 
     public event System.Action OnDeath;
     protected Vector3 forwardDir;
+
+    private Wall attackTarget;
+
     public float AttackSpeed
     {
         set => currentAtkSpeed = Mathf.Max(0, value);
@@ -55,15 +57,16 @@ public class Enemy : MonoBehaviour
     {
         
     }
+    void Update()
+    {
+        animator.SetFloat("Speed", currentSpeed);
+        FindTarget();
+    }
     public void SetUp(List<Transform> wps)
     {
         currentSpeed = speed;
         currentAtkSpeed = attackSpeed;
 
-        transformAttackRange = enemyAttackRangeObj.GetComponent<Transform>();
-        enemyAttackRange = enemyAttackRangeObj.GetComponent<EnemyAttackRange>();
-        enemyAttackRange.OnFindWall += Attack;
-        enemyAttackRange.OnMissWall += () => SetMoving(true);
 
         rigidbody2D = GetComponent<Rigidbody2D>();
         animator = GetComponent<Animator>(); animator.SetFloat("attackSpeed", currentAtkSpeed);
@@ -117,16 +120,7 @@ public class Enemy : MonoBehaviour
             }    
         }
     }
-    void OnTriggerEnter2D(Collider2D coll)
-    {
-        if (isDead == false)
-        {
-            if (coll.gameObject.tag == "Weapon")
-            {//�浹�� ������Ʈ�� weapon�϶�
-            }
-        }
-       
-    }
+
     IEnumerator Move()
     {
         while (currentWayPointIdx < wayPoints.Count)
@@ -135,12 +129,6 @@ public class Enemy : MonoBehaviour
             {
                 Vector3 dir = (wayPoints[currentWayPointIdx].position - transform.position).normalized;
                 forwardDir = dir;
-                //���ݹ��� �ö��̴� ���� ��ȯ
-                float roValue = 180;
-                if (dir.x <= 0) roValue = 90;
-                roValue += (90 * dir.x);
-                roValue += (90 * -dir.y);
-                transformAttackRange.rotation = Quaternion.Euler(new Vector3(0, 0,roValue));
 
                 animator.SetFloat("MoveX", dir.x);
                 animator.SetFloat("MoveY", dir.y);
@@ -171,18 +159,13 @@ public class Enemy : MonoBehaviour
         yield return new WaitForSeconds(0.1f);        
     }*/
 
-    public void Attack()
-    {
 
-        SetMoving(false);
-        
-    }
     private void AttackEvent()
     {//attack �ִϸ��̼� ����� �̺�Ʈ �޼���� ȣ���
-        Wall wall = enemyAttackRange.GetAttckTarget();
-        if (wall != null)
+
+        if (attackTarget!= null)
         {
-            wall.AddHp(-attackDmg);   
+            attackTarget.AddHp(-attackDmg);   
         }
     }
     private void SetMoving(bool value)
@@ -206,22 +189,31 @@ public class Enemy : MonoBehaviour
             {
                 OnDeath();
             }
-        
-        
 
+    }
+    private void FindTarget()
+    {
+        RaycastHit hitinfo;
+        Debug.DrawRay(transform.position, forwardDir * hitRange, Color.green);
+        if (Physics.Raycast(transform.position, forwardDir, out hitinfo, hitRange, layerMask))
+        {
+            if (attackTarget == null)
+            {
+                attackTarget = hitinfo.transform.gameObject.GetComponent<Wall>();
+                SetMoving(false);
+            }
+        }
+        else
+        {
 
+            SetMoving(true);
+            
+        }
+        
     }
     private void DieEvent()
     {//death �ִϸ��̼� ����� �̺�Ʈ �޼���� ȣ���.
         Destroy(gameObject);
-    }
-
-
-    // Update is called once per frame
-    void Update()
-    {
-        animator.SetFloat("Speed", currentSpeed);
- 
     }
 
     public void ResetAttackSpeed()
